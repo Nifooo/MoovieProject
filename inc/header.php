bbcce98d2cc87438647d46e82dd7971b91481d8a
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-12-04 12:53:23 +0100

    affichage des 100 film en aléatoire sur l'accueil avec test pour pagination

--- inc/header.php
+++ inc/header.php
@@ -5,25 +5,17 @@
     <title><?php if (!empty($title)) {
             echo $title;
         } else {
-            echo 'Exo';
+            echo '';
         } ?></title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 <header>
-    <?php //debug($_SESSION);?>
+
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <?php if (!isLogged()) { ?>
-                <li><a href="register.php">Inscriptions</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="randomproject.php">Random</a></li>
-            <?php } else { ?>
-                <li><a href="logout.php">Logout</a></li>
 
-                <li>Bonjour <?php echo $_SESSION['login']['pseudo'] ?> !</li>
-            <?php } ?>
         </ul>
     </nav>
 
--- index.php
+++ index.php
@@ -3,10 +3,56 @@ session_start();
 require('inc/pdo.php');
 require('function/function.php');
 $title = 'Home Page';
-debug($_SESSION);
-include('inc/header.php');
-?>
-    <h1>Home</h1>
+$errors = array();
+$succes = false;
 
+//Select=colonne; FROM= table; WHERE -> col1 = valeur; AND col2 = valeur2; ORDER BY = col ASC/DESC ; LIMIT = combien;
+
+$page = (!empty($_GET['page']) ? $_GET['page'] : 1);
+$limite = 100;
+$offset = ($page - 1) * $limite;
+$sql = "SELECT * FROM movies_full
+ORDER BY RAND()
+ LIMIT $limite 
+ OFFSET $offset ";
+$query = $pdo->prepare($sql);
+
+$query->execute();
+$movies = $query->fetchAll();
+include ('inc/header.php');
+
+?>
+<?php foreach ($movies as $movie) {
+    ; ?>
+
+    <h1>Titre : <?= $movie['title']; ?></h1>
+
+    <img src="posters/<?php echo $movie['id'] ?>.jpg" alt="<?= $movie['title']; ?>">
+
+
+<?php } ?>
+
+<?php
+
+
+// Partie "Boucle"
+while ($element = $query->fetch()) {
+    // C'est là qu'on affiche les données  :)
+}
+
+// Partie "Liens"
+?>
+<?php
+if ($page > 1){
+    echo '<a href="index.php?page=' . ($page - 1) . '" class="btn btn-primary">Précédent</a>';
+}
+
+?>
+
+<?php
+if (empty($page)){
+    echo '<a href="index.php?page=' . ($page + 1) . '" class="btn btn-primary">Suivant</a>';
+}
+?>
 <?php
 include('inc/footer.php');
