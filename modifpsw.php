da5516ae589400b66f831dc7f740a101e9d50354
JsKriss <57392242+JsKriss@users.noreply.github.com>
2019-12-11 10:35:05 +0100

    modif de mdp

---
+++ modifpsw.php
@@ -0,0 +1,82 @@
+<?php
+
+require('inc/pdo.php');
+require('function/function.php');
+$title = 'Home Page';
+$errors = array();
+$succes = false;
+if (!empty($_GET['token']) && $_GET['email']) {
+
+    $token = clean($_GET['token']);
+    $email = clean($_GET['email']);
+    $email = urldecode($_GET['email']);
+    $sql = "SELECT email,token FROM users WHERE email = :email AND token = :token";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
+    $query->bindValue(':token', $token, PDO::PARAM_STR);
+
+    $query->execute();
+    $user = $query->fetch();
+    if(!empty($user)) {
+
+        if (!empty($_POST['sumbitted'])) {
+            $password1 = clean($_POST['']);
+            $password2 =  clean($_POST['']);
+            if (!empty($password1)) {
+                if ($password1 != $password2) {
+                    $errors['password'] = 'Les deux mot de passe doivent être identique';
+                } elseif (mb_strlen($password1) <= 5) {
+                    $errors['password'] = 'Min 6 caractères';
+//
+
+                }
+                die('404');
+
+
+            }else{
+                $errors['password'] = 'Veuillez renseigné ce champ';
+            }
+            if (count($errors)==0){
+                $hashpsw = password_hash($password1, PASSWORD_BCRYPT);
+                $token = generateRandomString(200);
+                $sql = "UPDATE users SET password = :password, token = :token WHERE email = :email";
+                $query = $pdo->prepare($sql);
+                die('404');
+
+
+                $query->bindValue(':email', $email, PDO::PARAM_STR);
+                $query->bindValue(':password', $hashpsw, PDO::PARAM_STR);
+                $query->bindValue(':token', $token, PDO::PARAM_STR);
+
+                $query->execute();
+                die('404');
+            }
+        } else {
+
+
+        }
+
+
+    }
+}else{
+
+}
+include('inc/header.php');
+?>
+    <h1>Modifier mot de passe</h1>
+    <form action="" method="post">
+        <label for="password1">Mot de passe *</label>
+        <input type="password" id="password1" name="password1" value="">
+        <p class="error"><?php if (!empty($errors['password'])) {
+                echo $errors['password'];
+            } ?></p>
+
+
+        <label for="password2">Confirmation mdp *</label>
+        <input type="password" id="password2" name="password2" value="">
+
+        <input type="submit" name="submitted" value="Envoyer">
+    </form>
+
+<?php
+include('inc/footer.php');
